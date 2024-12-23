ceb5daf94b7ecc9dd76586826d70ed8c8047c47a
Prince Koomson <princekoomson03@gmail.com>
2024-12-23 03:58:19 -0800

    Initial commit

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// filepath: /c:/Users/Prince Koomson/Downloads/WebTech_FInal-main/server.js
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
